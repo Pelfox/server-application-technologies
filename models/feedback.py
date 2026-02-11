149adc6d9d26b5d5872ab0efcfddf4621378e107
Aleksey Demchenkov <contact@pelfox.dev>
2026-02-11 15:20:20 +0300

    feat: task 2.2

--- models/feedback.py
+++ models/feedback.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel
+from re import IGNORECASE, search
+
+from pydantic import BaseModel, Field, field_validator
 
 
 class Feedback(BaseModel):
-    name: str
-    message: str
+    name: str = Field(min_length=2, max_length=50)
+    message: str = Field(min_length=10, max_length=500)
+
+    @field_validator("message")
+    @classmethod
+    def validate_message(cls, value: str) -> str:
+        pattern = r"\b(кринж\w*|рофл\w*|вайб\w*)\b"
+        if search(pattern, value, IGNORECASE):
+            raise ValueError("Использование недопустимых слов")
+        return value
 
 
 class FeedbackResponse(BaseModel):
