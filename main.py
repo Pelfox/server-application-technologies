dbb096cf669c0635ec5e42dc40e0ad4771d23e8f
Aleksey Demchenkov <contact@pelfox.dev>
2026-02-11 15:06:00 +0300

    feat: task 1.4

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from starlette.responses import FileResponse
 
+from models.user import User
+
 app = FastAPI()
+existing_user = User(id=1, name="Ваше Имя и Фамилия")
 
 
 @app.get("/")
@@ -12,3 +15,8 @@ async def index():
 @app.post("/calculate")
 async def calculate(num1: int, num2: int) -> dict:
     return {"result": num1 + num2}
+
+
+@app.get("/users")
+async def users() -> User:
+    return existing_user
