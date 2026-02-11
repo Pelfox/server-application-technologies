2cabd468cabb303c8c6400f0c9b73dc2ed0c3276
Aleksey Demchenkov <contact@pelfox.dev>
2026-02-11 15:00:36 +0300

    feat: task 1.3

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return FileResponse("./assets/index.html")
+
+
+@app.post("/calculate")
+async def calculate(num1: int, num2: int) -> dict:
+    return {"result": num1 + num2}
